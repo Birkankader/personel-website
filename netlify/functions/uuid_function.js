4d9a75f0f3dc30273f9aa1f5622a79706fb3d42b
Birkan Kader <birkankader@outlook.com>
2025-09-22 14:41:24 +0300

    Update uuid_function.js

--- netlify/functions/uuid_function.js
+++ netlify/functions/uuid_function.js
@@ -1,13 +1,20 @@
 const xml2js = require("xml2js");
 
 exports.handler = async (event) => {
-  console.log("Method:", event.httpMethod);
-  console.log("Headers:", event.headers);
   console.log("Body:", event.body);
-
   return {
     statusCode: 200,
-    body: "OK",
+    headers: { "Content-Type": "application/xml" },
+    body: `<?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Status</key>
+        <string>OK</string>
+      </dict>
+    </plist>`
   };
 };
+
 
